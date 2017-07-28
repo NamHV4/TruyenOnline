2bdb05319e626c2d8ac4e066a6e7dbd79431f542
namhv4 <utnam.ltmt1@gmail.com>
2017-07-28 23:34:45 +0700

    Add Base

---
+++ app/src/main/java/com/codedao/truyenonline/presenter/BasePresenter.java
@@ -0,0 +1,8 @@
+package com.codedao.truyenonline.presenter;
+
+/**
+ * Created by utnam on 7/28/2017.
+ */
+
+public class BasePresenter {
+}
---
+++ app/src/main/java/com/codedao/truyenonline/view/BaseView.java
@@ -0,0 +1,8 @@
+package com.codedao.truyenonline.view;
+
+/**
+ * Created by utnam on 7/28/2017.
+ */
+
+public class BaseView {
+}
