4379610741f3ae10da0d156999f20246714650ce
namhv4 <utnam.ltmt1@gmail.com>
2017-08-08 19:56:46 +0700

    Add BaseActivity and BaseFragment

---
+++ app/src/main/java/com/codedao/truyenonline/base/BaseActivity.java
@@ -0,0 +1,20 @@
+package com.codedao.truyenonline.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by utnam on 8/8/2017.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerEventView();
+    }
+
+    protected abstract void registerEventView();
+
+}
