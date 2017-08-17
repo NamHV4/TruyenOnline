be78c677f686e6d96177ee989e680b906544ae5c
namhv4 <utnam.ltmt1@gmail.com>
2017-08-17 22:43:01 +0700

    Create Screen 1

--- app/src/main/java/com/codedao/truyenonline/view/fragment/IndexFragment.java
+++ app/src/main/java/com/codedao/truyenonline/view/fragment/IndexFragment.java
@@ -13,17 +13,24 @@ import android.view.ViewGroup;
 
 import com.codedao.truyenonline.R;
 import com.codedao.truyenonline.adapter.HorizontalAdapter;
+import com.codedao.truyenonline.adapter.IndexAdapter;
+import com.codedao.truyenonline.model.Index;
+import com.codedao.truyenonline.model.Truyen;
 
 import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class IndexFragment extends Fragment implements HorizontalAdapter.IOnItemClickListener {
+public class IndexFragment extends Fragment implements HorizontalAdapter.IOnItemClickListener,IndexAdapter.IOnItemClickListener {
 
     private RecyclerView mRecyclerView;
     private HorizontalAdapter horizontalAdapter;
     private ArrayList<String> horizontalList;
+    private RecyclerView mRecyclerViewIndex;
+    private IndexAdapter mIndexAdapter;
+    private ArrayList<Index> mIndexArrayListl;
+
     public IndexFragment() {
         // Required empty public constructor
     }
@@ -34,11 +41,10 @@ public class IndexFragment extends Fragment implements HorizontalAdapter.IOnItem
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_index, container, false);
 
-        addSuggestionFragment();
 
-
+        mRecyclerViewIndex = view.findViewById(R.id.rcIndex);
         mRecyclerView = view.findViewById(R.id.rcView);
-        horizontalList=new ArrayList<>();
+        horizontalList = new ArrayList<>();
         horizontalList.add("TRUYỆN NGƯỜI LỚN");
         horizontalList.add("TRUYỆN TRANH");
         horizontalList.add("TRUYỆN NGÔN LÙ");
@@ -54,33 +60,57 @@ public class IndexFragment extends Fragment implements HorizontalAdapter.IOnItem
         horizontalList.add("ITEM 5");
         horizontalList.add("TRUYỆN CƯỜI");
         horizontalList.add("ITEM 5");
-        horizontalAdapter=new HorizontalAdapter(horizontalList, this);
-
+        horizontalAdapter = new HorizontalAdapter(horizontalList, this);
 
 
         LinearLayoutManager horizontalLayoutManagaer
                 = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
         mRecyclerView.setLayoutManager(horizontalLayoutManagaer);
-
-
         mRecyclerView.setAdapter(horizontalAdapter);
+
+
+        mIndexArrayListl = new ArrayList<>();
+        for (int i = 0; i <= 1000; i++) {
+            mIndexArrayListl.add(new Index("Truỵện mới nhất", listtruen()));
+        }
+        mIndexAdapter=new IndexAdapter(mIndexArrayListl,getContext(),this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),
+                LinearLayoutManager.VERTICAL, false);
+        mRecyclerViewIndex.setLayoutManager(linearLayoutManager);
+        mRecyclerViewIndex.setAdapter(mIndexAdapter);
+
+
         return view;
     }
 
-    private void addSuggestionFragment() {
-        FragmentManager fragmentManager= getFragmentManager();
-        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.rcvSuggest,new Screen2Fragment());
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
-    }
+
 
 
     @Override
     public void onClick(int position) {
-        FragmentManager fragmentManager= getFragmentManager();
-        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.conten,new Screen3Fragment());
+        transitScreen(new Screen3Fragment());
+    }
+
+    private ArrayList<Truyen> listtruen() {
+        ArrayList<Truyen> truyens = new ArrayList<>();
+        Truyen truyen = new Truyen();
+        truyen.setmTenTruyen("Hen Tai");
+        truyen.setmAvatar(R.drawable.avata);
+        for (int i = 0; i <= 100; i++) {
+            truyens.add(truyen);
+        }
+        return truyens;
+    }
+
+    @Override
+    public void onClickItem(int position) {
+       transitScreen(new Screen2Fragment());
+    }
+
+    private void transitScreen(Fragment fragment){
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.conten, fragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
