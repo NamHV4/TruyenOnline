73f034c7981c82b13d553e5ba9fcc670e69bf996
MinhQuang <minhquang0094@gmail.com>
2017-08-18 17:27:34 +0700

    Fix raw screen 2 item layout

--- app/src/main/java/com/codedao/truyenonline/view/MainActivity.java
+++ app/src/main/java/com/codedao/truyenonline/view/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         progressBar=findViewById(R.id.progressBar);
         getSupportActionBar().hide();
-        CountDownTimer countDownTimer=new CountDownTimer(2000,2000) {
+        CountDownTimer countDownTimer=new CountDownTimer(1000,1000) {
             @Override
             public void onTick(long l) {
 
