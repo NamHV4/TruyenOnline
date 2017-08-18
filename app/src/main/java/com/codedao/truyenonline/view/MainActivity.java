0e7d5964e151d1a38e04a7bde2d2e3d9262e9541
MinhQuang <minhquang0094@gmail.com>
2017-08-18 15:29:05 +0700

    Add raw splash screen

--- app/src/main/java/com/codedao/truyenonline/view/MainActivity.java
+++ app/src/main/java/com/codedao/truyenonline/view/MainActivity.java
@@ -1,16 +1,32 @@
 package com.codedao.truyenonline.view;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import com.codedao.truyenonline.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar=findViewById(R.id.progressBar);
+        getSupportActionBar().hide();
+        CountDownTimer countDownTimer=new CountDownTimer(2000,2000) {
+            @Override
+            public void onTick(long l) {
 
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(MainActivity.this,Screen1.class));
+            }
+        };
+        countDownTimer.start();
     }
 }
